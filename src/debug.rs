e20f962de5d77555fbab91d511c8a4348606dd32
Alan Vardy <alanvardy@gmail.com>
2025-08-09 18:40:53 -0700

    chore: show last 5 of token in verbose output

--- src/debug.rs
+++ src/debug.rs
@@ -1,9 +1,27 @@
 use crate::{color, config::Config};
 
+const TOKEN_SUFFIX_LENGTH: usize = 5;
+const TOKEN_LENGTH: usize = 40;
+
 // Print a debug statement if in verbose mode
 pub fn maybe_print(config: &Config, text: String) {
     if config.verbose.unwrap_or_default() || config.args.verbose {
         print(text)
+    }
+}
+
+// Print config with token redacted to console if in verbose mode
+// Everything but the last 5 characters are turned into x's before being printed to console
+pub fn maybe_print_redacted_config(config: &Config) {
+    if config.verbose.unwrap_or_default() || config.args.verbose {
+        let token = config.token.as_ref().map(|t| {
+            let redacted = "x".repeat(TOKEN_LENGTH - TOKEN_SUFFIX_LENGTH);
+            let suffix = t.len().saturating_sub(TOKEN_SUFFIX_LENGTH);
+            format!("{}{}", redacted, &t[suffix..])
+        });
+        let mut config = config.clone();
+        config.token = token;
+        print(format!("{config:#?}"));
     }
 }
 
