3b34954be42f3d5a5159753f5cd22ef51665e17c
Jesse <stacksjb@users.noreply.github.com>
2025-08-12 11:39:21 -0600

    chore: commitlint & dependabot fixes
    dependabot & commitlint fixes

--- .commitlint.config.mjs
+++ .commitlint.config.mjs
@@ -2,6 +2,8 @@
 export default {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'body-max-line-length': [2, 'always', 250], //Override the default body line length to 250 characters
+        'header-max-length': [2, 'always', 250], //Override the default header line length to 100 characters
+        'body-max-line-length': [2, 'always', 250], //Override the default body line length to 250 character
+        'body-max-length': [0, 'always', 5000], //Disable the default body length
     },
 };
